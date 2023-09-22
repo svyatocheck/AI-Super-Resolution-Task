4a4b3645c6db3ec4bde73333eda53b5a3bcacdc2
Ivanov_SV <svyatwork2@gmail.com>
2023-09-22 09:08:27 +0300

    Init streamlit app framework

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def launch_app():
+    st.title("Super Resolution")
+    st.subheader("About Super-resolution problem solving for the Digital Breakthrough hackathon")
+
+
+if __name__ == "__main__":
+    launch_app()
